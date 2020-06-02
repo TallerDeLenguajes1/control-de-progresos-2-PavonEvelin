f0a568a57e6b9b85cd126c1133ec134094fba174
PavonEvelin <evelinluz17@gmail.com>
2020-06-02 16:00:10 -0300

    agrgada clase personaje

---
+++ TPDeProgresos/MiniJuegoDeRol/Personaje.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MiniJuegoDeRol
+{
+    class Personaje
+    {
+        private string nombre, apellido, apodo;
+        private DateTime fechaNac;
+        private int edad, salud;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Apodo { get => apodo; set => apodo = value; }
+        public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public int Salud { get => salud; set => salud = value; }
+
+        public int calcularEdad(DateTime fechaNac)
+        {
+            
+        }
+    }
+}
---
+++ TPDeProgresos/MiniJuegoDeRol/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MiniJuegoDeRol
+{
+    class Program
+    {
+        enum valorMax
+        {
+            velocidadMax = 10,
+            destrezaMax = 5,
+            fuerza = 10,
+            nivel = 10,
+            armadura = 10
+        }
+        static void Main(string[] args)
+        {
+            Personaje nuevoPersonaje = new Personaje();
+        }
+    }
+}
