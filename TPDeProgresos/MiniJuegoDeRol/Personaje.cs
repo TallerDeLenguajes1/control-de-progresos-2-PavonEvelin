fb11d5e2fa97588ccb92b18340155ef1a258fa1f
PavonEvelin <evelinluz17@gmail.com>
2020-06-02 16:11:19 -0300

    agregado proyecto de control

--- TPDeProgresos/MiniJuegoDeRol/Personaje.cs
+++ TPDeProgresos/MiniJuegoDeRol/Personaje.cs
@@ -4,11 +4,20 @@ using System.Text;
 
 namespace MiniJuegoDeRol
 {
+    enum valorMax
+    {
+        velocidadMax = 10,
+        destrezaMax = 5,
+        fuerza = 10,
+        nivel = 10,
+        armadura = 10
+    }
     class Personaje
     {
         private string nombre, apellido, apodo;
         private DateTime fechaNac;
         private int edad, salud;
+        string tipo;
 
         public string Nombre { get => nombre; set => nombre = value; }
         public string Apellido { get => apellido; set => apellido = value; }
@@ -16,6 +25,7 @@ namespace MiniJuegoDeRol
         public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
         public int Edad { get => edad; set => edad = value; }
         public int Salud { get => salud; set => salud = value; }
+        public string Tipo { get => tipo; set => tipo = value; }
 
         public int calcularEdad(DateTime fechaNac)
         {
