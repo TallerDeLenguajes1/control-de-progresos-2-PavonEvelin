601c835152d2924b9ad7cbf04b9cb8465edac6c2
PavonEvelin <evelinluz17@gmail.com>
2020-06-04 16:17:11 -0300

    .

--- TPDeProgresos/MiniJuegoDeRol/Personaje.cs
+++ TPDeProgresos/MiniJuegoDeRol/Personaje.cs
@@ -4,32 +4,52 @@ using System.Text;
 
 namespace MiniJuegoDeRol
 {
-    enum valorMax
+    /*
+    public enum tipos
     {
-        velocidadMax = 10,
-        destrezaMax = 5,
-        fuerza = 10,
-        nivel = 10,
-        armadura = 10
-    }
-    class Personaje
+        goblin,
+        orco,
+        duende
+    }*/
+
+    public class Personaje
     {
-        private string nombre, apellido, apodo;
+        private string nombre, apodo;
+        private string tipo;
+        private int velocidad, destreza, fuerza, nivel, armadura, edad;
         private DateTime fechaNac;
-        private int edad, salud;
-        string tipo;
 
         public string Nombre { get => nombre; set => nombre = value; }
-        public string Apellido { get => apellido; set => apellido = value; }
         public string Apodo { get => apodo; set => apodo = value; }
+        public string[] Tipo { get => tipo; set => tipo = value; }
+        public int Velocidad { get => velocidad; set => velocidad = value; }
+        public int Destreza { get => destreza; set => destreza = value; }
+        public int Fuerza { get => fuerza; set => fuerza = value; }
+        public int Nivel { get => nivel; set => nivel = value; }
+        public int Armadura { get => armadura; set => armadura = value; }
         public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
         public int Edad { get => edad; set => edad = value; }
-        public int Salud { get => salud; set => salud = value; }
-        public string Tipo { get => tipo; set => tipo = value; }
 
-        public int calcularEdad(DateTime fechaNac)
+        public int calcularEdad()
         {
-            
+            return edad = DateTime.Now.Year - fechaNac.Year;
+        }
+
+        public void cargarDatosAleatorios(int velocidad1, int destreza1, int fuerza1, int nivel1, int armadura1)
+        {
+            velocidad = velocidad1;
+            destreza = destreza1;
+            fuerza = fuerza1;
+            nivel = nivel1;
+            armadura = armadura1;
+        }
+
+        public void cargarDatos(string nombre1, string apodo1, string tipo1,)
+        {
+            nombre = nombre1;
+            apodo = apodo1;
+            tipo = tipo1;
+            fechaNac = 
         }
     }
 }
--- TPDeProgresos/MiniJuegoDeRol/Program.cs
+++ TPDeProgresos/MiniJuegoDeRol/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using MiniJuegoDeRol;
 
 namespace MiniJuegoDeRol
 {
@@ -7,6 +8,26 @@ namespace MiniJuegoDeRol
         static void Main(string[] args)
         {
             Personaje nuevoPersonaje = new Personaje();
+            int velocidad = new Random().Next(1 , 11);
+            int destreza = new Random().Next(1, 6);
+            int fuerza = new Random().Next(1, 11);
+            int nivel = new Random().Next(1, 11);
+            int armadura = new Random().Next(1, 11);
+
+            string nombre, apodo;
+            Console.WriteLine("Ingrese su nombre\n");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese su apodo\n");
+            apodo = Console.ReadLine();
+            Console.WriteLine("Ingrese el numero de su tipo de jugador: goblin, orco o duende");
+            string tipo = Console.ReadLine();
+
+            string fechaNac;
+            Console.WriteLine("Ingrese su fecha de nacimiento con el siguiente formato: 17/03/00\n");
+            fechaNac = Console.ReadLine();
+
+            nuevoPersonaje.cargarDatosAleatorios(velocidad, destreza, fuerza, nivel, armadura);
+
         }
     }
 }
